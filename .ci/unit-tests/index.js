f0ddb3d96fc246ced6234bd090a346061ea31e4f
Segmentational <60639231+Segmentational@users.noreply.github.com>
2021-11-07 02:20:12 -0600

    [Development] - Unit Testing

--- .ci/unit-tests/index.js
+++ .ci/unit-tests/index.js
@@ -1,9 +1,10 @@
 import { Commands, Resolve } from "./../command.js";
 
 const Inputs = [
+    git submodule foreach git pull && cd .ci/cluster-adapter && git pull --force && npm install . && npm run test
     "git submodule init",
     "git submodule foreach git pull",
-    "cd \"$(git rev-parse --show-toplevel)\"/.ci/archive",
+    "cd \"$(git rev-parse --show-toplevel)\"/.ci/cluster-adapter",
     "npm install . && npm run test && echo $?"
 ];
 
