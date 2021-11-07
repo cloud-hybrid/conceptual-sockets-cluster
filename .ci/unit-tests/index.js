79d4dede849b7bb39662fdf11454140b2d32ac3e
Segmentational <60639231+Segmentational@users.noreply.github.com>
2021-11-07 02:04:11 -0600

    [Development] - Unit Tests

---
+++ .ci/unit-tests/index.js
@@ -0,0 +1,17 @@
+import { Commands, Resolve } from "./../command.js";
+
+const Inputs = [
+    "git diff --exit-code || git add --all",
+    "git diff --exit-code || git commit --message \"Pre-Unit-Testing\"",
+    "git diff --exit-code || git push",
+    "git submodule init",
+    "git config --file \".gitconfig\" pull.rebase false",
+    "git submodule update --recursive",
+    "git submodule foreach git checkout --force",
+    "git submodule foreach git pull origin",
+    "git checkout --force",
+    "cd \"$(git rev-parse --show-toplevel)\"/.ci/archive",
+    "npm install . && npm run test && echo $?"
+];
+
+Resolve(Commands(Inputs));
